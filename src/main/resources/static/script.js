1810b113b9518ec69e2f94cbf126de38249d8e93
kaiki nattan dos santos guedes <kaikinattansg@gmail.com>
2025-02-01 14:04:26 -0300

    comentários de auxilio

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,6 +3,7 @@ function calcular() {
     const operador = document.getElementById('operador').value;
     const operando2 = document.getElementById('operando2').value;
 
+    //enviar informações para back
     fetch('/calcular', {
         method: 'POST',
         headers: {
@@ -10,10 +11,14 @@ function calcular() {
         },
         body: JSON.stringify({ operando1, operador, operando2 }),
     })
+    
+    //recuperando mensagens assim que o servidor responde
     .then(response => response.json())
     .then(data => {
         document.getElementById('resultado').innerText = `Resultado: ${data.resultado}`;
     })
+
+    //capturar erros de conexão com o servidor
     .catch(error => {
         console.error('Erro:', error);
     });
